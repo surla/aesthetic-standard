0a17877d039cf17a5c15f9cd3a94c645fdf594cc
Joseph Surla <josephsurla@gmail.com>
2017-12-14 18:08:20 -0800

    Add typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+    `gatsby-plugin-react-helmet`
+  ],
+  
 }
---
+++ src/utils/typography.js
@@ -0,0 +1,5 @@
+import Typography from "typography";
+
+const typography = new Typography({ baseFontSize: "26px" });
+
+export default typography;
